ce1cae66071beb484ed6ae37cc4127e37791b371
CodyJReed <cody_j_reed@proton.me>
2026-05-17 18:09:05 +0000

    lambda, cb && arrow function exercise

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
-for ($i = 1; $i <= 100; $i++) {
-  if ($i % 3 == 0 && $i % 5 == 0) {
-    echo 'fizzbuzz' . '<br>';
-  } else if ($i % 3 == 0) {
-    echo 'fizz' . '<br>';
-  } else if ($i % 5 == 0) {
-    echo 'buzz' . '<br>';
-  } else {
-    echo $i . '<br>';
-  }
-}
+$numbers = array(1, 2, 3, 4, 5);
+$cb = function ($number) {
+return $number * $number;
+};
+
+$squaredNumbers = array_map($cb, $numbers);
+
+$add = fn ($a, $b) => $a + $b;
+
+$sum = $add(2, 3);
+
+echo $sum;
